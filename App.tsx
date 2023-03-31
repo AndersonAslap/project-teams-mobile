594332cb8221abe87feaf8831196d08f4a9634a1
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-03-31 15:13:04 -0300

    app

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar, View } from 'react-native'
 import { Groups } from '@screens/Groups'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { ThemeProvider } from 'styled-components'
+
+import theme from './src/theme'
+import { Loading } from '@components/Loading'
+import { StatusBar } from 'react-native'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
+
   return (
-    <View>
-      <StatusBar />
-      <Groups />
-    </View>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
+    </ThemeProvider>
   )
 }
